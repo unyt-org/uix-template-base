d5d5b39f0b9e935f348a1d85c03a08f8aa571eda
Jonas Strehle <jonas@strehles.info>
2024-10-12 12:35:00 +0200

    Remove invalid stuff

--- frontend/entrypoint.ts
+++ frontend/entrypoint.ts
@@ -1,10 +1,2 @@
-/**
- * Frontend entrypoint:
- * This module provides a default export that defines the UI that is created on the frontend
- * when a page is visited
- */
-
-export default {
-	// show frontend-rendered page on /frontend
-	'/frontend': import("common/page.tsx") 
-}
+import { type Entrypoint } from "uix/providers/entrypoints.ts";
+export default "Hello, UIX!" satisfies Entrypoint;
